5dd94e0c43e8ddf39e15cdffe35b3568b413c00f
Mazy <mazy_ios@163.com>
2019-02-17 10:54:35 +0800

    init project

---
+++ FeSpinnerSwift/AppDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FeSpinnerSwift
+//
+//  Created by Mazy on 2019/2/17.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
