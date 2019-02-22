5f9de03b07477e3b3c3d9ed05b3d174ab3eccbe5
Mazy <mazy_ios@163.com>
2019-02-22 15:41:46 +0800

    add wifi man resource

---
+++ FeSpinnerSwift/FeSpinner/Type/WifiManHub/FeWifiManHub.swift
@@ -0,0 +1,15 @@
+//
+//  FeWifiManHub.swift
+//  FeSpinnerSwift
+//
+//  Created by Mazy on 2019/2/22.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class FeWifiManHub: UIView {
+
+    
+
+}
