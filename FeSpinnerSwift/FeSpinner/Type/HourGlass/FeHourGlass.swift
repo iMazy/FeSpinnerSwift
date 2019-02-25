5d62b3f2e8fec4ccba4dc0ae3d1f779f87e92aaf
Mazy <mazy_ios@163.com>
2019-02-25 15:27:57 +0800

    add pure hour class

---
+++ FeSpinnerSwift/FeSpinner/Type/HourGlass/FeHourGlass.swift
@@ -0,0 +1,14 @@
+//
+//  FeHourGlass.swift
+//  FeSpinnerSwift
+//
+//  Created by Mazy on 2019/2/25.
+//  Copyright © 2019 Mazy. All rights reserved.
+//
+
+import UIKit
+
+class FeHourGlass: UIView {
+
+
+}
